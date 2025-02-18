ab027e5632b821f74e87aa5f1acda29c7c848379
Nikolin <nikolinandoni80@gmail.com>
2025-02-18 17:45:00 +0100

    updated data prices

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -47,7 +47,7 @@ export default function Shop() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch("/products.json")
+        const response = await fetch("/products1.json")
         if (!response.ok) {
           throw new Error("Failed to fetch products")
         }
